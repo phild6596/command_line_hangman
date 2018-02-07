6b216de8d3a1aed5cf22a526f791ca90106c6d81
phild6596 <phild6596@gmail.com>
2018-02-06 16:13:21 -0800

    added boolean, and returns a '_'if they type something (hopefully).

---
+++ letter.js
@@ -0,0 +1,16 @@
+var Letter = function (letters){
+    this.letter = letters;
+    this.appear = false;
+
+    this.letterRender = function() {
+        if(this.letter === ''){
+            this.appear = true;
+            return ' ';
+        }if (this.appear ===false) {
+            return '_';
+        }else {
+            return this.letter;
+        }
+    }
+}
+module.exports = Letter;
